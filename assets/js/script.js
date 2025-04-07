4230681816762110e553d03ac229ae2c151d4c98
Gabriel Albuquerque Souza <106005629+gabriel-souza-developer@users.noreply.github.com>
2025-04-07 13:09:56 -0300

    Add files via upload

--- assets/js/script.js
+++ assets/js/script.js
@@ -67,15 +67,15 @@ document.addEventListener('DOMContentLoaded', function() {
     // Define os valores padrão para o problema relatado e a tratativa
     function setCategoryDefaults(categoria) {
         const defaults = {
-            instalacao_ativacao: { problema: 'Solicitação de instalação/ativação.', tratativa: 'Realizada instalação/ativação/provisionamento utilizando ERP Voalle, UNM2000 ou TERMINUS.' },
-            manutencao: { problema: 'Solicitação de manutenção.', tratativa: 'Realizados procedimentos para atendimento de manutenção utilizando ERP Voalle, UNM2000 ou TERMINUS.' },
-            reparo: { problema: 'Solicitação de reparo.', tratativa: 'Identificado o problema e realizadas as medidas corretivas (provisionamento/desprovisionamento) utilizando ERP Voalle, UNM2000 ou TERMINUS.' },
-            troca_endereco: { problema: 'Solicitação de troca de endereço.', tratativa: 'Realizado desprovisionamento da ONU no endereço antigo e provisionamento da ONU novo endereço utilizando ERP Voalle, UNM2000 ou TERMINUS.' },
-            verificar_cto: { problema: 'Solicitação de verificação da CTO.', tratativa: 'Verificação da CTO realizada utilizando ERP Voalle, UNM2000 ou TERMINUS.' },
-            desprovisionar_provisionar: { problema: 'Solicitação de desprovisionamento e provisionamento de ONU.', tratativa: 'Desprovisionamento e provisionamento realizados utilizando ERP Voalle, UNM2000 ou TERMINUS.' },
-            verificar_conexao: { problema: 'Solicitação de verificação de conexão.', tratativa: 'Verificação de conexão realizada, com análise de parâmetros, utilizando ERP Voalle, UNM2000 ou TERMINUS.' },
-            apenas_desprovisionar: { problema: 'Solicitação de desprovisionamento.', tratativa: 'Desprovisionamento realizado utilizando ERP Voalle, UNM2000 ou TERMINUS.' },
-            apenas_provisionar: { problema: 'Solicitação de provisionamento.', tratativa: 'Provisionamento realizado utilizando ERP Voalle, UNM2000 ou TERMINUS.' }
+            instalacao_ativacao: { problema: 'Solicitação de instalação/ativação.', tratativa: 'Realizada instalação/ativação/provisionamento da ONU/ONT utilizando ERP Voalle, UNM2000 ou TERMINUS. Após o procedimento o técnico/solicitante foi orientado a testar conexão do cliente.' },
+            manutencao: { problema: 'Solicitação de manutenção.', tratativa: 'Realizados procedimentos para atendimento de manutenção utilizando ERP Voalle, UNM2000 ou TERMINUS. Após o procedimento o técnico/solicitante foi orientado a testar conexão do cliente.' },
+            reparo: { problema: 'Solicitação de reparo.', tratativa: 'Identificado o problema e realizadas as medidas corretivas (provisionamento/desprovisionamento) utilizando ERP Voalle, UNM2000 ou TERMINUS. Após o procedimento o técnico/solicitante foi orientado a testar conexão do cliente.' },
+            troca_endereco: { problema: 'Solicitação de troca de endereço.', tratativa: 'Realizado desprovisionamento da ONU/ONT no endereço antigo e provisionamento da ONU/ONT em novo endereço utilizando ERP Voalle, UNM2000 ou TERMINUS. Após o procedimento o técnico/solicitante foi orientado a testar conexão do cliente.' },
+            verificar_cto: { problema: 'Solicitação de verificação da CTO.', tratativa: 'Verificação da CTO realizada utilizando ERP Voalle, UNM2000 ou TERMINUS. Técnico foi informado sobre as portas disponíveis e indisponíveis da CTO' },
+            desprovisionar_provisionar: { problema: 'Solicitação de desprovisionamento e provisionamento da ONU/ONT.', tratativa: 'Desprovisionamento e provisionamento realizados utilizando ERP Voalle, UNM2000 ou TERMINUS. Após o procedimento o técnico/solicitante foi orientado a testar conexão do cliente.' },
+            verificar_conexao: { problema: 'Solicitação de verificação de conexão.', tratativa: 'Verificação de conexão realizada com análise de parâmetros e orientações de testes como: Reiniciar ONU/ONT e aguarda a estabilização dos LEDs, verificar se as configurações da WAN estão corretas (modo bridge ou router, autenticação PPPoE com IPv4 & IPv6), medir o nível de sinal de potência óptica, conferência de atenunação na fibra, testes de conexão com cabo Ehternet ou Wi-fi, análise de histórico de autenticação do cliente... As orientações foram repassadas ao técnico utilizando ERP Voalle, UNM2000 ou TERMINUS.' },
+            apenas_desprovisionar: { problema: 'Solicitação de desprovisionamento.', tratativa: 'Desprovisionamento realizado utilizando ERP Voalle, UNM2000 ou TERMINUS. Após o procedimento o técnico/solicitante foi orientado a testar conexão do cliente.' },
+            apenas_provisionar: { problema: 'Solicitação de provisionamento.', tratativa: 'Provisionamento realizado utilizando ERP Voalle, UNM2000 ou TERMINUS. Após o procedimento o técnico/solicitante foi orientado a testar conexão do cliente.' }
         };
 
         problemaRelatadoTextarea.value = defaults[categoria]?.problema || '';
@@ -105,7 +105,7 @@ document.addEventListener('DOMContentLoaded', function() {
         const evidencias = document.querySelector('input[name="evidencias"]:checked');
         const evidenciasStr = evidencias ? evidencias.value.toUpperCase() : '';
 
-        let registroCompleto = `Protocolo Operações: ${protocolo}\n\nProblema Relatado: ${problemaRelatadoTextarea.value}\n\nTratativa: ${tratativaTextarea.value}\n\nTécnico/solicitante enviou evidências: ${evidenciasStr}`;
+        let registroCompleto = `Protocolo de operações: ${protocolo}\n\nProblema relatado: ${problemaRelatadoTextarea.value}\n\nTratativa: ${tratativaTextarea.value}\n\nTécnico/solicitante enviou evidências: ${evidenciasStr}`;
 
         registroTextarea.value = registroCompleto;
         registroGeradoSection.style.display = 'block';
